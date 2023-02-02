645a7d618ffeeb869653b0e3090141df2fe4359a
Shivangi <shivangit1111@gmail.com>
2023-02-02 21:55:06 +0530

    Revert "Revert "This is our fourth commit""
    This reverts commit 18cf9c1ac2d8da26db400a4cbd4d2920c19f8e88.

--- app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
@@ -9,8 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is CONTAINED in MY Second commit")
-
-        today is good day
+        ADFFFggjhkjkjkll
     }
 
 }
