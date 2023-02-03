bbbff1366183e60a9dd0870516cd05a4448a766f
Shivangi <shivangit1111@gmail.com>
2023-02-03 16:52:59 +0530

    Change 1 for commit

--- app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is CONTAINED in MY Second commit")
+        println("This is change1 for merge")
 
     }
 
