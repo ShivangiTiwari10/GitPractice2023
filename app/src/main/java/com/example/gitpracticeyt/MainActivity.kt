9c45dc3f43d02c67e8c5ee575c16847b08cb77d9
Shivangi <shivangit1111@gmail.com>
2023-02-03 19:00:04 +0530

    final merge

--- app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is CONTAINED in MY Second commit")
-        println("Change 2 commit")
+        println("Change 3 commit")
         println("After Changes 2.1")
 
         println("This line will be merged")
