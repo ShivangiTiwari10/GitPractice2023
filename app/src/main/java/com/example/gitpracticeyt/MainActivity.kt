ca13e6604687a1bd0c9dd28ddf2c7139d3e3939b
Shivangi <shivangit1111@gmail.com>
2023-02-03 17:25:54 +0530

    After Change 2.1 commit

--- app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
         println("This is CONTAINED in MY Second commit")
         println("Change 2 commit")
+        println("After Changes 2.1")
     }
 
 }
